a1c83fa405aca87e99ddf4ce1e6d89d2653c40ec
chenlonglong <chenlonglong.qiyi.com>
2018-10-10 10:46:23 +0800

    add mac src

---
+++ platform/ios/LLLog/LLLog/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LLLog
+//
+//  Created by multimedia on 2018/9/14.
+//  Copyright © 2018年 ___multiMedia___. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
